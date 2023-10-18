47deb43fd5e2e033461c88219bfc8c4abb1c9063
Jarrod Overson <jsoverson@gmail.com>
2023-10-18 09:47:22 -0400

    Update wick.rb

--- Formula/wick.rb
+++ Formula/wick.rb
@@ -1,9 +1,9 @@
 class Wick < Formula
-  desc "a WebAssembly application framework based on wasmtime"
+  desc "WebAssembly application framework based on wasmtime"
   homepage "https://candle.dev"
   url "https://github.com/candlecorp/wick/archive/refs/tags/0.16.0.tar.gz"
   sha256 "0324216d6f36b0a79e949f12b80e5ac202afc6d5342cde14a0acf759e1c1fbf0"
-  license "Elastic License 2.0"
+  license "Elastic-2.0"
   head "https://github.com/candlecorp/wick.git", branch: "main"
 
   depends_on "protobuf" => :build
