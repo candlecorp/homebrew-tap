dca57a5ce8fc2fce2a4f6b8c53ece124f1eca1b3
Jarrod Overson <jsoverson@gmail.com>
2023-10-18 09:37:42 -0400

    Update for 0.16.0

--- Formula/wick.rb
+++ Formula/wick.rb
@@ -1,8 +1,8 @@
 class Wick < Formula
   desc "Wick - a flow-based application framework for WebAssembly"
   homepage "https://candle.dev"
-  url "https://github.com/candlecorp/wick/archive/refs/tags/0.5.0.tar.gz"
-  sha256 "68c9246e43e2aaa6ba390ba8cf081d8203eb38cd04c8232e7e2855cb1c491b10"
+  url "https://github.com/candlecorp/wick/archive/refs/tags/0.16.0.tar.gz"
+  sha256 "0324216d6f36b0a79e949f12b80e5ac202afc6d5342cde14a0acf759e1c1fbf0"
   license "Elastic License 2.0"
   head "https://github.com/candlecorp/wick.git", branch: "main"
 
